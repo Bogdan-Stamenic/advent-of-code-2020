dbfff4999afdb4dfefad028e53eddcd358442447
Bogdan Stamenic <bogdan.stamenic@hotmail.com>
2020-12-01 18:53:45 +0100

    fix1 day1.

--- Makefile
+++ Makefile
@@ -2,10 +2,12 @@ DepsDay1 = file_to_vec.o find_sum_2020.o
 
 day1: day1.o $(DepsDay1)
 	g++ -o day1 $(DepsDay1) day1.o
+	mkdir -p bin
+	mv day1 bin/day1
 	make clean
 
 day1.o: day1.cpp
-	g++ -c day1.cpp
+	g++ -c src/main/day1.cpp
 
 ## Object files compiled from src/
 file_to_vec.o: src/file_to_vec.cpp
---
+++ src/main/day1.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+#include <fstream>
+#include <cstdlib>
+#include <string>
+#include <vector>
+
+std::vector<int> file_to_int_vec(std::string file_name);
+std::vector<int> find_sum_2020p1(std::vector<int> int_vec);
+std::vector<int> find_sum_2020p2(std::vector<int> int_vec);
+
+std::vector<int> read_expense_report()
+{
+	return file_to_int_vec("expense_report.txt");
+}
+
+int main(int argc, char *argv[]){
+	std::vector<int> expenses = read_expense_report();
+	std::vector<int> sum_2020;
+
+	if ( ((argc==2) && (*argv[1]=='1'))||(argc==1)  ) {
+		std::vector<int> sum_2020 = find_sum_2020p1(expenses);
+		std::cout << "Answer:" << sum_2020[0]*sum_2020[1] << std::endl;
+	}else if(  (argc==2) && (*argv[1]=='2') ){
+		std::vector<int> sum_2020 = find_sum_2020p2(expenses);
+		std::cout << "Answer:" << sum_2020[0]*sum_2020[1]*sum_2020[2] << std::endl;
+	}else{
+		std::cout << "Error: Invalid argument. Must be \"1\" or \"2\"." << std::endl;
+		return 1;
+	}
+	return 0;
+}
