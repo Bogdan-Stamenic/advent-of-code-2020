fc8654f669782f68fc9b90fa3b8a6617605fec32
Bogdan Stamenic <bogdan.stamenic@hotmail.com>
2020-12-01 19:43:14 +0100

    fix2 day1

--- day1.cpp
+++ day1.cpp
@@ -1,12 +1,14 @@
 #include <cstdlib>
 #include <fstream>
 #include <iostream>
+#include <span>
+#include <stdexcept>
 #include <string>
 #include <vector>
 
 std::vector<int> file_to_int_vec(std::string file_name);
-std::vector<int> find_sum_2020p1(std::vector<int> int_vec);
-std::vector<int> find_sum_2020p2(std::vector<int> int_vec);
+int find_sum_2020p1(std::span<int> int_vec);
+int find_sum_2020p2(std::span<int> int_vec);
 
 std::vector<int> read_expense_report() {
   return file_to_int_vec("expense_report.txt");
@@ -16,16 +18,13 @@ int main(int argc, char *argv[]) {
   std::vector<int> expenses = read_expense_report();
 
   if (((argc == 2) && (*argv[1] == '1')) || (argc == 1)) {
-    std::vector<int> sum_2020 = find_sum_2020p1(expenses);
-    std::cout << "Answer:" << sum_2020[0] * sum_2020[1] << std::endl;
+    auto sum_2020 = find_sum_2020p1(expenses);
+    std::cout << sum_2020 << std::endl;
   } else if ((argc == 2) && (*argv[1] == '2')) {
-    std::vector<int> sum_2020 = find_sum_2020p2(expenses);
-    std::cout << "Answer:" << sum_2020[0] * sum_2020[1] * sum_2020[2]
-              << std::endl;
+    auto sum_2020 = find_sum_2020p2(expenses);
+    std::cout << sum_2020 << std::endl;
   } else {
-    std::cout << "Error: Invalid argument. Must be \"1\" or \"2\"."
-              << std::endl;
-    return 1;
+    throw std::runtime_error{"Invalid argument!"};
   }
   return 0;
 }
--- src/find_sum_2020.cpp
+++ src/find_sum_2020.cpp
@@ -1,57 +1,29 @@
 #include <iostream>
+#include <span>
+#include <stdexcept>
 #include <string>
 #include <vector>
 
-std::vector<int> find_sum_2020p1(std::vector<int> int_vec)
-{
-	std::vector<int> nums;
-	for (auto i: int_vec) {
-		for (auto j: int_vec) {
-			if (i+j == 2020) {
-				nums.push_back(j);
-				break;
-			}
-		}
-		if (!nums.empty()) {
-			nums.push_back(i);
-			break;
-		}
-	}
-	std::cout << "Size of vector:" << nums.size() << std::endl;
-	std::cout << "Expenses that sum to 2020:" << std::endl;
-	for (auto i: nums) {
-		std::cout << i << std::endl;
-	}
-	return nums;
+int find_sum_2020p1(std::span<int> int_span) {
+  for (auto &i : int_span) {
+    for (auto &j : int_span) {
+      if (i + j == 2020) {
+        return i * j;
+      }
+    }
+  }
+  throw std::runtime_error{"Could not find entries."};
 }
 
-std::vector<int> find_sum_2020p2(std::vector<int> int_vec)
-{
-	std::vector<int> nums;
-	for (auto& i: int_vec) {
-		for (auto& j: int_vec) {
-			for (auto& k: int_vec) {
-				if (i+j+k == 2020) {
-					nums.push_back(k);
-					break;				
-				}
-			}
-			if (nums.size()==1) {
-				nums.push_back(j);
-				break;
-			}
-		}
-		if (nums.size()==2){
-			nums.push_back(i);
-			break;
-		}
-	}
-	std::cout << "Size of vector:" << nums.size() << std::endl;
-	std::cout << "Expenses that sum to 2020:" << std::endl;
-	for (auto i: nums) {
-		std::cout << i << std::endl;
-	}
-	return nums;
+int find_sum_2020p2(std::span<int> int_span) {
+  for (auto &i : int_span) {
+    for (auto &j : int_span) {
+      for (auto &k : int_span) {
+        if (i + j + k == 2020) {
+          return i * j * k;
+        }
+      }
+    }
+  }
+  throw std::runtime_error{"Could not find entries."};
 }
-
-
