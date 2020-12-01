0dd06463e42a6dcb34e3fa3cb3c078096a78d25f
Bogdan Stamenic <bogdan.stamenic@hotmail.com>
2020-12-01 21:09:57 +0100

    fix2 day1

--- src/find_sum_2020.cpp
+++ src/find_sum_2020.cpp
@@ -1,54 +1,29 @@
 #include <iostream>
 #include <span>
+#include <stdexcept>
 #include <string>
 #include <vector>
 
-std::span<int> find_sum_2020p1(std::span<int> int_span) {
-  std::span<int> nums;
-  for (auto i : int_span) {
-    for (auto j : int_span) {
+int find_sum_2020p1(std::span<int> int_span) {
+  for (auto &i : int_span) {
+    for (auto &j : int_span) {
       if (i + j == 2020) {
-        nums.push_back(j);
-        break;
+        return i * j;
       }
     }
-    if (!nums.empty()) {
-      nums.push_back(i);
-      break;
-    }
   }
-  std::cout << "Size of vector:" << nums.size() << std::endl;
-  std::cout << "Expenses that sum to 2020:" << std::endl;
-  for (auto i : nums) {
-    std::cout << i << std::endl;
-  }
-  return nums;
+  throw std::runtime_error{"Could not find entries."};
 }
 
-std::span<int> find_sum_2020p2(std::span<int> int_span) {
-  std::span<int> nums;
+int find_sum_2020p2(std::span<int> int_span) {
   for (auto &i : int_span) {
     for (auto &j : int_span) {
       for (auto &k : int_span) {
         if (i + j + k == 2020) {
-          nums.push_back(k);
-          break;
+          return i * j * k;
         }
       }
-      if (nums.size() == 1) {
-        nums.push_back(j);
-        break;
-      }
-    }
-    if (nums.size() == 2) {
-      nums.push_back(i);
-      break;
     }
   }
-  std::cout << "Size of vector:" << nums.size() << std::endl;
-  std::cout << "Expenses that sum to 2020:" << std::endl;
-  for (auto i : nums) {
-    std::cout << i << std::endl;
-  }
-  return nums;
+  throw std::runtime_error{"Could not find entries."};
 }
