29ab39e35e0b02c6b171135c107da0a54c55011f
Bogdan Stamenic <bogdan.stamenic@tum.de>
2020-12-03 16:12:56 +0100

    day3: part 1 with Kevin's style suggestions.

--- src/main/day3.cpp
+++ src/main/day3.cpp
@@ -8,35 +8,30 @@ std::vector<std::string> file_to_string_vec(std::string file_name);
 class DownhillToboggan {
 private:
   std::vector<std::string> m_downhill_slope;
-  int toboggan_x, toboggan_y = 0;
-  int crash_counter = 0;
+  int m_toboggan_x, m_toboggan_y = 0;
+  int m_crash_counter = 0;
   int m_finish_line;
 
-  bool m_is_there_a_tree(char snowbank) {
+  bool is_there_a_tree(char snowbank) {
     if (snowbank == '#') {
       return true;
-    } else {
-      return false;
     }
-    throw std::runtime_error("Error: something unexpexted happened.");
+    return false;
   }
 
-  bool m_did_i_crash(){
-      bool arg = m_is_there_a_tree(m_downhill_slope[toboggan_y][toboggan_x]);
+  bool did_i_crash(){
+      bool arg = is_there_a_tree(m_downhill_slope[m_toboggan_y][m_toboggan_x]);
       if (arg) {
           return true;
-      } else {
-          return false;
       }
+      return false;
   }
 
-  bool m_is_out_of_bounds(std::string str, int idx) {
+  bool is_out_of_bounds(std::string str, int idx) {
     if (idx > str.size()) {
       return true;
-    } else {
-      return false;
     }
-    throw std::runtime_error("Error: something unexpexted happened.");
+  return false;
   }
 
 public:
@@ -45,26 +40,26 @@ public:
 
   /* Move sled down and check for crashes. Positive y-increment for downhill and
    * positive x-increment for right. */
-  void m_downhill_increment(int x_inc = 3, int y_inc = 1) {
-    toboggan_y += y_inc;
-    if (m_is_out_of_bounds(m_downhill_slope[toboggan_y], toboggan_x + x_inc)) {
-      toboggan_x =
-          toboggan_x + x_inc - m_downhill_slope[toboggan_y].size();
+  void downhill_increment(int x_inc = 3, int y_inc = 1) {
+    m_toboggan_y += y_inc;
+    if (is_out_of_bounds(m_downhill_slope[m_toboggan_y], m_toboggan_x + x_inc)) {
+      m_toboggan_x =
+          m_toboggan_x + x_inc - m_downhill_slope[m_toboggan_y].size();
     } else {
-      toboggan_x = toboggan_x + x_inc;
+      m_toboggan_x = m_toboggan_x + x_inc;
     }
-    if(m_did_i_crash()){
-        crash_counter++;
+    if(did_i_crash()){
+        m_crash_counter++;
     }
   }
 
-  int m_downhill_toboggan(){
+  int downhill_toboggan(){
     for (int i = 0; i < m_downhill_slope.size(); i++) {
-        m_downhill_increment();
-        std::cout << "Increment " << i << " crashes : " << crash_counter << std::endl;
+        downhill_increment();
+        std::cout << "Increment " << i << " crashes : " << m_crash_counter << std::endl;
     }
-    std::cout << "Crash count is : " << crash_counter << std::endl;
-    return crash_counter;
+    std::cout << "Crash count is : " << m_crash_counter << std::endl;
+    return m_crash_counter;
   }
 };
 
@@ -80,7 +75,7 @@ int main(int argc, char *argv[]) {
   int finish_line = downhill_slope.size();
   std::cout << std::boolalpha;
   DownhillToboggan sled(downhill_slope);
-  sled.m_downhill_toboggan();
+  sled.downhill_toboggan();
 
   std::chrono::high_resolution_clock::time_point toc =
       std::chrono::high_resolution_clock::now();
