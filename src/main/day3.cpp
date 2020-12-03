01018c4d57d6f29581335301b9cd9b65a2c74765
Bogdan Stamenic <bogdan.stamenic@tum.de>
2020-12-03 10:15:20 +0100

    day3 temp1

--- src/main/day3.cpp
+++ src/main/day3.cpp
@@ -1,9 +1,28 @@
 #include <chrono>
+#include <iostream>
+#include <vector>
+#include <stdexcept>
+
+std::vector<std::string> file_to_string_vec(std::string file_name);
+
+bool is_there_a_tree (char snowbank){
+    if (snowbank == '#') {
+        return true;
+    }else{
+        return false;
+    }
+    throw std::runtime_error("Error: something unexpexted happened.");
+}
 
 int main(int argc, char *argv[])
 {
     std::chrono::high_resolution_clock::time_point tic = std::chrono::high_resolution_clock::now();
 
+    std::vector<std::string> downhill_slope = file_to_string_vec("day3_input.txt");
+    int finish_line = downhill_slope.size();
+    std::cout << std::boolalpha;
+    std::cout << "downhill_slope[0][3]: " << downhill_slope[0][3] << std::endl;
+    std::cout << "Crash into tree? : " << is_there_a_tree(downhill_slope[0][3]) << std::endl;
     /* insert code here */
 
     std::chrono::high_resolution_clock::time_point toc = std::chrono::high_resolution_clock::now();
