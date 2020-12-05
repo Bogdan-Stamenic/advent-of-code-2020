d8b59977fbdc980d4364d57b60a7b6f2137ac193
Bogdan Stamenic <bogdan.stamenic@tum.de>
2020-12-05 08:11:16 +0100

    temp

--- src/main/day4.cpp
+++ src/main/day4.cpp
@@ -24,11 +24,11 @@ private:
       /*insert code here*/
       if (messy_entry.find(":", idx) != std::string::npos) {
         std::string key = messy_entry.substr(idx, 3);
-        //std::cout << "Key : " << key << std::endl;
+        // std::cout << "Key : " << key << std::endl;
         /* should equal std::string::npos when at final entry --> */
         int eov = messy_entry.find(" ", idx);
         std::string value = messy_entry.substr(idx + 4, eov - (idx + 4));
-        //std::cout << "Value : " << value << std::endl;
+        // std::cout << "Value : " << value << std::endl;
         myMap.insert(std::pair<std::string, std::string>(key, value));
         idx += 5 + value.size();
         continue;
@@ -48,8 +48,11 @@ public:
         clean_creds.push_back(clean_cred_entry);
         clean_cred_entry.erase(clean_cred_entry.begin(),
                                clean_cred_entry.end());
+        //std::cout << "Empty entry! Moving to next map." << std::endl;
       } else {
         messy_entry_to_map(clean_cred_entry, messy_cred_entry);
+        //std::cout << "Appending to current map." << std::endl;
+
       }
     }
     // throw std::runtime_error("Error: something unexpected happened.");
@@ -63,17 +66,24 @@ public:
 #pragma omp for schedule(dynamic)
     for (auto c : clean_creds) {
       for (auto f : fields) {
-          std::cout << (c.find(f) == c.end()) << std::endl;
-        if (c.find("bleck") == c.end()) {
-            std::cout << "f : " << f << std::endl;
+        //std::cout << (c.count(f) == 0) << std::endl;
+        if (c.count(f) == 0) {
+          //std::cout << "f : " << f << std::endl;
         } else {
-            std::cout << "Bleck" << std::endl;
+          //std::cout << "Bleck" << std::endl;
         }
       }
       valid_count += 1;
     }
     std::cout << "Valid passports : " << valid_count << std::endl;
   }
+
+  // void print_keys(){
+  //    std::vector<std::map<std::string, std::string>>::iterator it =
+  //    clean_creds.begin(); for (it; it < clean_creds.end(); ++it) {
+  //        std::cout << it->first << std::endl;
+  //    }
+  //}
 
   PassportProcessor() {}
   ~PassportProcessor() {}
