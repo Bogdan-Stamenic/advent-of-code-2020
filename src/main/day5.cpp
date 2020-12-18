3356938eba7ca688c7448c57084da9ced8734332
Bogdan Stamenic <bogdan.stamenic@tum.de>
2020-12-18 20:40:41 +0100

    day5: finished part 1.

--- src/main/day5.cpp
+++ src/main/day5.cpp
@@ -38,7 +38,7 @@ int decode_column(std::string column_code) {
     return sum;
 }
 
-SeatPos letter_code_to_seat_num(std::string letter_code) {
+SeatPos letter_code_to_seat_pos(std::string letter_code) {
   unsigned int row, column;
   row = decode_row(letter_code.substr(0, 7));
   column = decode_column(letter_code.substr(7, 3));
@@ -46,14 +46,39 @@ SeatPos letter_code_to_seat_num(std::string letter_code) {
   return seat_pos;
 }
 
+std::vector<SeatPos> parse_seat_codes(std::vector<std::string> seat_codes) {
+    std::vector<SeatPos> parsed;
+    for (auto a: seat_codes) {
+        parsed.push_back(letter_code_to_seat_pos(a));
+    }
+    return parsed;
+}
+    
+int calc_seat_id(SeatPos seat_pos)
+{
+    int id = seat_pos.row * 8 + seat_pos.column;
+    return id;
+}
+
+int find_highest_id(std::vector<SeatPos> parsed) {
+    int highest = 0;
+    for (auto a: parsed) {
+        if (calc_seat_id(a) > highest) {
+            highest = calc_seat_id(a);
+        }
+    }
+    return highest;
+}
+
 int main(int argc, char *argv[]) {
   std::chrono::high_resolution_clock::time_point t1 =
       std::chrono::high_resolution_clock::now();
 
   /* insert code here */
-  std::string test = "FBFBBFFRLR";
-  SeatPos seat_pos = letter_code_to_seat_num(test);
-  std::cout << "Row: " << seat_pos.row << "\nColumn: " << seat_pos.column << std::endl;
+  std::vector<std::string> seat_codes = file_to_string_vec("day5_input.txt");
+  std::vector<SeatPos> parsed = parse_seat_codes(seat_codes);
+  int highest = find_highest_id(parsed);
+  std::cout << "The highest seat id is : " << highest << std::endl;
 
   std::chrono::high_resolution_clock::time_point t2 =
       std::chrono::high_resolution_clock::now();
