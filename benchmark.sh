7e9d172783af9d05ff3d0e095d213474de99f229
Bogdan Stamenic <bogdan.stamenic@hotmail.com>
2023-10-21 14:15:47 +0200

    Updated benchmark.sh.

--- benchmark.sh
+++ benchmark.sh
@@ -1,8 +1,22 @@
 #!/bin/bash
-if [ "$#" -eq 1 ]; then
-	hyperfine --warmup 10 --min-runs 15 "./${1}" >> hyperfine_benchmark_results.log
-elif  [ "$#" -eq 2 ]; then
-	hyperfine --warmup 10 --min-runs 15 "./${1} ${2}" >> hyperfine_benchmark_results.log
+LOGFILE=hyperfine_benchmark_results.log
+make $1
+make $1_O3
+
+echo "**************************************************" >> $LOGFILE
+if [ -f ./$1 ]; then
+	if [ "$#" -eq 1 ]; then
+		hyperfine --warmup 10 --min-runs 15 --shell=none "./${1} 1" >> "$LOGFILE"
+		hyperfine --warmup 10 --min-runs 15 --shell=none "./${1} 2" >> "$LOGFILE"
+		hyperfine --warmup 10 --min-runs 15 --shell=none "./${1}_O3 1" >> "$LOGFILE"
+		hyperfine --warmup 10 --min-runs 15 --shell=none "./${1}_O3 2" >> "$LOGFILE"
+		echo "Done."
+	elif  [ "$#" -eq 2 ]; then
+		hyperfine --warmup 10 --min-runs 15 --shell=none "./${1}_O3 ${2}" >> "$LOGFILE"
+		echo "Done."
+	else
+		echo "Something went wrong."
+	fi
 else
-	echo "Something went wrong."
+	echo "File $1 not found!"
 fi
