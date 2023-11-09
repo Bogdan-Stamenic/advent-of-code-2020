472b0cc7433aec925aefc1ca89b999728b18b5c6
Bogdan Stamenic <bogdan.stamenic@hotmail.com>
2023-11-09 11:31:16 +0100

    day15 : tried making more efficient solution.

--- src/main/day15.cpp
+++ src/main/day15.cpp
@@ -9,7 +9,8 @@
 #include <utility>
 #include <vector>
 
-#define MAX_NUM (1 << 25)
+#define MAX_ARR (1 << 25)
+#define MAX_HYB (1 << 12)
 
 int64_t elf_memory_game(std::string input, uint64_t N) {
 	/* Read starting numbers */
@@ -26,9 +27,9 @@ int64_t elf_memory_game(std::string input, uint64_t N) {
 	starting_numbers.push_back(num);
 	/* Init */
 	std::vector<int64_t> memory =
-		std::vector<int64_t>(MAX_NUM, -1);
+		std::vector<int64_t>(MAX_ARR, -1);
 	std::vector<int64_t> memory_prev =
-		std::vector<int64_t>(MAX_NUM, -1);
+		std::vector<int64_t>(MAX_ARR, -1);
 	int64_t turn_counter = 0;
 	for (auto num : starting_numbers) {
 		memory[num] = turn_counter;
@@ -77,7 +78,6 @@ int64_t elf_memory_game_hashmap(std::string input, uint64_t N) {
 	int64_t last_num = starting_numbers.back();
 	int64_t nnum = 0;
 	while (turn_counter < N) {
-		//std::cout << turn_counter << " : " << memory.at(nnum) << '\n';
 		if (memory_prev.at(last_num) == -1) {
 			nnum = 0;
 		} else {
@@ -94,6 +94,67 @@ int64_t elf_memory_game_hashmap(std::string input, uint64_t N) {
 	}
 	return nnum;
 }
+
+class NumHolder {
+	public:
+		int64_t& operator[](std::size_t idx){
+			if (idx < MAX_HYB) {
+				return  m_common_small_numbers[idx];
+			}
+			return m_rare_big_numbers[idx];
+		}
+		int64_t& at(std::size_t idx){
+			if (idx < MAX_HYB) {
+				return  m_common_small_numbers.at(idx);
+			}
+			return m_rare_big_numbers.at(idx);
+		}
+	private:
+		std::vector<int64_t> m_common_small_numbers =
+			std::vector<int64_t>(MAX_HYB,-1);
+		std::unordered_map<int64_t, int64_t> m_rare_big_numbers{};
+};
+
+int64_t elf_memory_game_hybrid(std::string input, uint64_t N) {
+	/* Read starting numbers */
+	std::vector<int64_t> starting_numbers;
+	int idx_fst = 0;
+	int idx_lst = 0;
+	while (input.find(',',idx_fst) != std::string::npos) {
+		idx_lst = input.find(',',idx_fst);
+		int64_t num = std::stol(input.substr(idx_fst,idx_lst - idx_fst));
+		starting_numbers.push_back(num);
+		idx_fst = idx_lst + 1;
+	}
+	int64_t num = std::stol(input.substr(idx_fst,input.size() - idx_fst));
+	starting_numbers.push_back(num);
+	/* Init */
+	NumHolder memory;
+	NumHolder memory_prev;
+	int64_t turn_counter = 0;
+	for (auto num : starting_numbers) {
+		memory[num] = turn_counter;
+		std::cout << turn_counter << " : " << memory.at(num) << '\n';
+		turn_counter++;
+	}
+	/* Solve */
+	int64_t last_num = starting_numbers.back();
+	int64_t nnum = 0;
+	while (turn_counter < N) {
+		std::cout << turn_counter << " : " << memory.at(nnum) << '\n';
+		if (memory_prev.at(last_num) == -1) {
+			nnum = 0;
+		} else {
+			nnum = memory.at(last_num) - memory_prev.at(last_num);
+		}
+		memory_prev[nnum] = memory.at(nnum);
+		memory[nnum] = turn_counter;
+		last_num = nnum;
+		turn_counter++;
+	}
+	return nnum;
+}
+
 
 
 int main(int argc, char *argv[]) {
@@ -128,10 +189,14 @@ int main(int argc, char *argv[]) {
 		std::vector<std::string> input_line_by_line = file_to_string_vec("input/day15_dev.txt");
 		int64_t answer = elf_memory_game(input_line_by_line[0], 2020);
 		std::cout << answer << std::endl;
+		answer = elf_memory_game_hybrid(input_line_by_line[0], 10);
+		std::cout << answer << std::endl;
 	} else if ((argc == 2) && (*argv[1] == '4')) {
 		/* developement */
 		std::vector<std::string> input_line_by_line = file_to_string_vec("input/day15_dev.txt");
-		int64_t answer = elf_memory_game_hashmap(input_line_by_line[0], 30000000);
+		int64_t answer = elf_memory_game(input_line_by_line[0], 30000000);
+		std::cout << answer << std::endl;
+		answer = elf_memory_game_hybrid(input_line_by_line[0], 30000000);
 		std::cout << answer << std::endl;
 	} else {
 		std::cout << "Error: Invalid argument. Must be \"1\" or \"2\" for solver or \"3\" or \"4\" for developement." << std::endl;
